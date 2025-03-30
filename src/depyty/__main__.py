6c5887cd769eb7094b0b212bde7a328e5e2f4bc5
Niclas van Eyk <niclas.eyk@gmail.com>
2025-03-30 22:23:00 +0200

    refactor source_file_collection
    divided up into dependencies.py and pyproject.py

--- src/depyty/__main__.py
+++ src/depyty/__main__.py
@@ -1,9 +1,8 @@
-from os import getcwd
-from pathlib import Path
 import sys
-
 from glob import glob
 from itertools import chain
+from os import getcwd
+from pathlib import Path
 
 from depyty.environment import get_available_modules_by_name
 from depyty.reporting.console import ConsoleReporter
@@ -23,7 +22,7 @@ or wherever else you store your deployment artifacts in your monorepo.
 
 def main():
     # First we inspect the environment, to see what packages are installed.
-    available_modules_by_name = get_available_modules_by_name()
+    available_modules = get_available_modules_by_name()
 
     if (
         len(sys.argv) <= 1
@@ -38,10 +37,8 @@ def main():
     # import, and if their imprts are properly declared.
     globs = chain(*(glob(pyproject_glob) for pyproject_glob in sys.argv[1:]))
     source_packages = parse_source_packages(globs)
-
-    violations = check_source_files(
-        iter_source_files_with_context(source_packages, available_modules_by_name)
-    )
+    source_files = iter_source_files_with_context(source_packages, available_modules)
+    violations = check_source_files(source_files)
 
     ConsoleReporter(Path(getcwd())).report(violations)
 
